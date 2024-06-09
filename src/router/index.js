8c5317b3d4a5a0ee89fc39cccfaea91bc4c152b7
liang <lwy.work@outlook.com>
2024-06-10 02:38:19 +0800

    配置路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-// 组件
-import HomePage from '../views/HomePage.vue'
-import Note from '../views/Note.vue'
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      component: HomePage
+      component: () => import('@/views/HomePage.vue')
     },
     {
       path: '/note/:id',
-      component: Note
+      component: () => import('@/views/Note.vue')
     }
   ]
 })
