0b9156a8dd5c6ef716e78f0feeff0842a2f591b1
liang <lwy.work@outlook.com>
2024-06-10 17:11:31 +0800

    新增笔记源码

--- src/stores/modules/Project.js
+++ src/stores/modules/Project.js
@@ -12,6 +12,9 @@ export const useProjectStore = defineStore('projectStore', () => {
     小兔鲜电商项目: { url: 'https://github.com/liang-Ryan/rabbit-shopping' },
     GBFwiki贺图汇总: {
       url: 'https://gbf.huijiwiki.com/wiki/%E8%B4%BA%E5%9B%BE%E6%B1%87%E6%80%BB'
+    },
+    个人笔记源码: {
+      url: 'https://github.com/liang-Ryan/Mypage-source'
     }
   })
 
